f1ddd23ef307079150ff1d8cbf1a3c7497f39fc3
Brice Zakra <31992444+bricezakra@users.noreply.github.com>
2022-06-17 12:26:34 -0500

    ec2-instance.tf file updated

--- ec2-instance.tf
+++ ec2-instance.tf
@@ -1,3 +1,10 @@
+# PREREQUISITES:
+## 1 - Ensure you have "DEFAULT-VPC" in that respective region
+## 2 - Ensure "AMI" you are provisioning exists in that region. If not, update "AMI ID" 
+## 3 - Verify your AWS Credentials in **$HOME/.aws/credentials
+
+
+
 # Terraform Settings Block
 terraform {
     required_providers{
@@ -19,4 +26,6 @@ resource "aws_instance" "ec2demo" {
     ami = "ami-0cff7528ff583bf9a" # Amazon Linux in us-east-1, update as per your region
     instance_type = "t2.micro"
 }
+
+
 
